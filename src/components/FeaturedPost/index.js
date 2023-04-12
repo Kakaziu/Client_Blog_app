253b74b284a4879ad58665c2e86e47f9c6845e1f
Kauã <kaua.borba03@gmail.com>
2023-04-12 20:26:10 -0300

    fix

--- src/components/FeaturedPost/index.js
+++ src/components/FeaturedPost/index.js
@@ -1,19 +1,12 @@
 import React, { useEffect, useState } from 'react'
+import PropTypes from 'prop-types'
 import './style.css'
-import { useDispatch, useSelector } from 'react-redux'
-import { getPostsRequest } from '../../store/modules/post/postActions'
 
-const FeaturedPost = () =>{
+const FeaturedPost = (props) =>{
 
-  const dispatch = useDispatch()
-  const postsState = useSelector(state => state.PostReducer)
-  const { posts } = postsState
   const [randomPost, setRandomPost] = useState({})
   const [postsLoader, setPostsLoader] = useState(false)
-
-  useEffect(() =>{
-    dispatch(getPostsRequest)
-  }, [])
+  const { posts } = props
 
   useEffect(() =>{
     if(posts.length > 0){
@@ -28,39 +21,6 @@ const FeaturedPost = () =>{
     return posts[random]
   }
 
-  function formatDescription(description){
-    if(description.length > 200){
-      description.slice(200)
-
-      return description + '...'
-    }
-  }
-
-  function formatDate(date){
-    const data = new Date() //eslint-disable-line
-    const actualYear = data.getFullYear()
-    const actualMonth = data.getMonth() + 1
-    const actualDay = data.getDate()
-
-    const creationYear = Number(date.slice(0, 4))
-    const creationMonth = Number(date.slice(5, 7))
-    const creationDay = Number(date.slice(8, 10))
-
-    if(actualYear !== creationYear){
-      return `a ${actualYear - creationYear} anos atrás`
-    }
-
-    if(actualMonth !== creationMonth){
-      return `a ${actualMonth - creationMonth} meses atrás`
-    }
-
-
-
-    return `a ${actualDay - creationDay} dias atrás`
-  }
-
-  console.log(formatDate('2023-04-12T02:00:59.000Z'))
-
   return(
     <>
       {postsLoader && (
@@ -69,16 +29,23 @@ const FeaturedPost = () =>{
           <div className="content-featured-post">
             <div>
               <h1>{randomPost.title}</h1>
-              <span>{formatDate(randomPost.created_at)}</span>
+              <span>{props.formatDate(randomPost.created_at)}</span>
             </div>
 
-            <p>{formatDescription(randomPost.description)}</p>
+            <p>{props.formatDescription(randomPost.description)}</p>
           </div>
         </div>
       )}
       <hr className="line-post" />
     </>
   )
+}
+
+
+FeaturedPost.PropTypes = {
+  formatDate: PropTypes.string,
+  formatDescription: PropTypes.string,
+  posts: PropTypes.object
 }
 
 export default FeaturedPost
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,13 +1,54 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../../components/Header'
 import FeaturedPost from '../../components/FeaturedPost'
 import Posts from '../../components/Posts'
+import { getPostsRequest } from '../../store/modules/post/postActions'
+import { useDispatch, useSelector } from 'react-redux'
+
 
 const Home = () =>{
+
+  const dispatch = useDispatch()
+  const postsState = useSelector(state => state.PostReducer)
+  const { posts } = postsState
+
+  useEffect(() =>{
+    dispatch(getPostsRequest)
+  }, [])
+
+  function formatDate(date){
+    const data = new Date() //eslint-disable-line
+    const actualYear = data.getFullYear()
+    const actualMonth = data.getMonth() + 1
+    const actualDay = data.getDate()
+
+    const creationYear = Number(date.slice(0, 4))
+    const creationMonth = Number(date.slice(5, 7))
+    const creationDay = Number(date.slice(8, 10))
+
+    if(actualYear !== creationYear){
+      return `a ${actualYear - creationYear} anos atrás`
+    }
+
+    if(actualMonth !== creationMonth){
+      return `a ${actualMonth - creationMonth} meses atrás`
+    }
+
+    return `a ${actualDay - creationDay} dias atrás`
+  }
+
+  function formatDescription(description){
+    if(description.length > 200){
+      description.slice(200)
+
+      return description + '...'
+    }
+  }
+
   return(
     <section id="home">
       <Header/>
-      <FeaturedPost/>
+      <FeaturedPost formatDate={formatDate} formatDescription={formatDescription} posts={posts}/>
       <Posts/>
     </section>
   )
