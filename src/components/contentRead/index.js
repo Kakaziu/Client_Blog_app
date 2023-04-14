b43d27cd815f9805bdf638d7d6cab734bb86a869
Kauã <kaua.borba03@gmail.com>
2023-04-14 00:28:41 -0300

    finish-project-beta

--- src/components/contentRead/index.js
+++ src/components/contentRead/index.js
@@ -1,29 +1,77 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import './style.css'
+import api from '../../services/api'
+
 
 function ContentRead(){
+
+  const { id } = useParams()
+  const [caughtPost, setCaugthPost] = useState(null) //eslint-disable-line
+  const [userPost, setUserPost] = useState(null)//eslint-disable-line
+  const [separatedParagraphs, setSeparateParagraphs] = useState([])
+
+  useEffect(() =>{
+    getPost(id)
+  }, [])
+
+  useEffect(() =>{
+    if(caughtPost){
+      getUserPost(caughtPost.create_by)
+      separateParagraphs()
+    }
+  }, [caughtPost])
+
+  async function getPost(id){
+    try{
+      const response = await api.get(`/posts/${id}`)
+
+      if(response.status === 200){
+        setCaugthPost(response.data)
+      }
+    }catch(e){
+      return null
+    }
+  }
+
+  async function getUserPost(id){
+    try{
+      const response = await api.get(`/users/${id}`)
+
+      if(response.status === 200){
+        setUserPost(response.data)
+      }
+    }catch(e){
+      return null
+    }
+  }
+
+  function formatDate(date){
+    const data = date.slice(0, 10)
+    const arrayDate = data.split('-')
+
+    return `${arrayDate[2]}/${arrayDate[1]}/${arrayDate[0]}`
+  }
+
+  function separateParagraphs(){
+    const arrayParagraph = caughtPost.description.split('///')
+
+    setSeparateParagraphs(arrayParagraph)
+  }
+
   return(
     <div className="content-read">
-      <h1>Gabigol</h1>
-      <span><p>Autor: Kauã Borba</p>  <p>02/02/2023</p></span>
-      <img src='http://localhost:3333/images/1681431249542.jpg'/>
+      <h1>{ caughtPost && caughtPost.title }</h1>
+      <span><p>Autor: {userPost && userPost.name}</p>  <p>Criado em: {caughtPost && formatDate(caughtPost.created_at)}</p></span>
+      <img src={caughtPost && caughtPost.photo_post_url}/>
       <div className='paragraphs-read'>
-        <p>
-          Gabriel Barbosa Almeida, mais conhecido como Gabigol, é um jogador de futebol brasileiro nascido
-          em São Bernardo do Campo, São Paulo, em 30 de agosto de 1996. Ele é atualmente um dos principais
-          jogadores do Flamengo e da seleção brasileira.
-        </p>
-        <p>
-          Gabigol iniciou sua carreira no Santos, clube em que foi revelado e onde jogou de 2013 a 2016.
-          Durante sua passagem pelo clube paulista, ele conquistou o Campeonato Paulista de 2015 e foi o
-          artilheiro do Brasileirão de 2018.
-        </p>
-        <p>
-        Em 2016, Gabigol foi vendido para a Inter de Milão, da Itália, mas não teve uma boa passagem pelo
-        clube europeu, sendo emprestado para o Benfica, de Portugal, e para o Santos novamente. Em 2019,
-        ele foi emprestado ao Flamengo e teve um desempenho excepcional, conquistando a torcida rubro-negra
-        e se tornando um dos maiores ídolos da história recente do clube.
-        </p>
+        { caughtPost && separatedParagraphs.map(paragraph =>{
+          return(
+            <p key={paragraph.index}>
+              {paragraph}
+            </p>
+          )
+        }) }
       </div>
     </div>
   )
