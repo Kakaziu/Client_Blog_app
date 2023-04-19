1c70c1472210a49557da86a539ab407e6906c96b
Kauã <kaua.borba03@gmail.com>
2023-04-19 20:47:39 -0300

    mobile header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { AiOutlineMenu } from 'react-icons/ai'
 import PropTypes from 'prop-types'
 import './style.css'
 import { Link } from 'react-router-dom'
@@ -8,6 +9,7 @@ import { logout } from '../../store/modules/user/userAction'
 const Header = (props) =>{
   const [showLinks, setShowLinks] = useState(false)
   const { user } = useSelector(state => state.UserReducer)
+  const [showMenuMobile, setShowMenuMobile] = useState(false)
   const dispatch = useDispatch()
 
   function handleLogout(){
@@ -32,21 +34,45 @@ const Header = (props) =>{
     <header className='header'>
       <div className='container'>
         <h2>MyBlog</h2>
-        { !user ?
-          <button onClick={() => setShowLinks(!showLinks)}>Cadastre-se</button> :
-          <div>
-            { user.is_admin ? <Link to='/panel'>Ir para o painel</Link> : ''}
-            <button onClick={handleLogout}>Logout</button>
-          </div>
+        <nav className='nav-desktop'>
+          { !user ?
+            <div className='menu'>
+              <button onClick={() => setShowLinks(!showLinks)}>Cadastre-se</button>
+            </div> :
+            <div className='menu'>
+              <span>Olá, {user.name}</span>
+              <hr></hr>
+              { user.is_admin ? <Link to='/panel'>Ir para o painel</Link> : ''}
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          }
+          { showLinks ? <div className={showLinks ? 'links links-show menu' : 'links menu'}>
+            <Link to='/register/reader'>Cadastre-se como leitor</Link>
+            <hr></hr>
+            <Link to='/register/author'>Cadastre-se como autor</Link>
+            <hr></hr>
+            <Link to='/login'>Login</Link>
+          </div> : <></>}
+        </nav>
+        <nav className='nav-mobile'>
+          <AiOutlineMenu className='menu-mobile-btn' size='30' onClick={() => setShowMenuMobile(!showMenuMobile)}/>
+          { !user ?
+            <div className={ showMenuMobile ? 'menu menuOn menu-no-user-mobile' : 'none' }>
+              <Link to='/register/reader'>Cadastre-se como leitor</Link>
+              <hr></hr>
+              <Link to='/register/author'>Cadastre-se como autor</Link>
+              <hr></hr>
+              <Link to='/login'>Login</Link>
+            </div> :
+            <div className={ showMenuMobile ? 'menu menuOn' : 'none' }>
+              <span>Olá, {user.name}</span>
+              <hr></hr>
+              { user.is_admin ? <Link to='/panel'>Ir para o painel</Link> : ''}
+              <button onClick={handleLogout}>Logout</button>
+            </div>
+          }
 
-        }
-        { showLinks ? <div className={showLinks ? 'links links-show' : 'links'}>
-          <Link to='/register/reader'>Cadastre-se como leitor</Link>
-          <hr></hr>
-          <Link to='/register/author'>Cadastre-se como autor</Link>
-          <hr></hr>
-          <Link to='/login'>Login</Link>
-        </div> : <></>}
+        </nav>
       </div>
     </header>
   )
